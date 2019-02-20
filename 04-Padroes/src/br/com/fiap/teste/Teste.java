9b2d83eeab4f7676e06093d43f23133874de7883
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-20 20:44:45 -0300

    Logger no projeto

--- 04-Padroes/src/br/com/fiap/teste/Teste.java
+++ 04-Padroes/src/br/com/fiap/teste/Teste.java
@@ -1,13 +1,19 @@
 package br.com.fiap.teste;
 
+import org.apache.log4j.Logger;
+
 import br.com.fiap.singleton.ConfiguracaoSingleton;
 
 public class Teste {
+	
+	private static Logger log = Logger.getLogger(Teste.class);	
 
 	public static void main(String[] args) {
+		log.trace("Iniando a Aplicação");
 		//Recuperar o idioma da aplicação
 		String idioma = ConfiguracaoSingleton
 					.getInstance().getProperty("idioma");
+		log.info("Recuperando a configuração de idioma");
 		System.out.println(idioma);
 	}
 	
--- 05-Loja-App-Refatorado/src/br/com/fiap/loja/TerminalConsulta.java
+++ 05-Loja-App-Refatorado/src/br/com/fiap/loja/TerminalConsulta.java
@@ -5,14 +5,20 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 import br.com.fiap.bo.EstoqueBO;
 import br.com.fiap.loja.exception.ProdutoNotFoundException;
 import br.com.fiap.loja.to.ProdutoTO;
 
+
 public class TerminalConsulta {
-
+	private static Logger log = Logger.getLogger(TerminalConsulta.class);
+	
 	public static void main(String[] args) {
-
+			
+		log.info("Iniciou a Aplicação");
+		log.debug("Build");
 		Scanner sc = new Scanner(System.in);
 		Calendar dataAtual = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -22,14 +28,17 @@ public class TerminalConsulta {
 
 			System.out.println("\nFIAPStore " + sdf.format(dataAtual.getTime()));
 			System.out.print("Digite o código: ");
+			log.warn("Vai trouxa");
 			int codigo = sc.nextInt();
 			try {
+				
 				ProdutoTO produto = estoqueBO.consultarProduto(codigo);
 				System.out.println(produto.getDescricao());
 				System.out.println("Preço: " + produto.getPreco());
 				System.out.println("Qtd: " + produto.getQtd());
 			} catch (ProdutoNotFoundException e) {
 				System.err.println("Produto nao encontrado");
+				log.error("Errou");
 				e.printStackTrace();
 			}
 			System.out.print("Sair? S/N ");			
